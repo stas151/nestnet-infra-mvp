64b81b222017864904144ca9bc5d55b77577eb4e
stas151 <121015337+stas151@users.noreply.github.com>
2025-04-10 11:26:32 +0300

    Update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,14 +1,8 @@
 #!/bin/bash
 
 echo "Docker и Docker Compose установлены. Перезагрузите терминал, если надо."
-
 echo "Развёртывание платформы NestNet MVP..."
 
-# Переход в корневую папку проекта (на один уровень выше скрипта)
-docker compose -f docker-compose.yml up --build -d || exit 1
-
-# Запуск docker-compose
-docker-compose up -d --build
+docker compose -f docker-compose.yml up --build -d
 
 echo "Развёртывание завершено!"
-
